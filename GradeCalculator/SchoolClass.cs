c1ff9e3ab9b56935b1dafa495e7a490eef7b45b6
andy-wojciechowski <31997141+andy-wojciechowski@users.noreply.github.com>
2017-12-07 11:49:17 -0600

    Commiting the GradeCalculator project files to git. Stubbed out object structure and a partially completed grade report

---
+++ GradeCalculator/SchoolClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+    public class SchoolClass
+    {
+        public string Name { get; set; }
+        public List<GradeCategory> categories { get; set; }
+        public List<Assignment> Assignments { get; set; }
+
+        public double CalculateFinalGrade()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, List<Assignment>> GetAssignmentsByCategory()
+        {
+            var result = new Dictionary<string, List<Assignment>>();
+            foreach(GradeCategory category in this.categories)
+            {
+                List<Assignment> relevantAssignments = this.Assignments.Where(x => x.category.Name.Equals(category.Name)).ToList();
+                result.Add(category.Name, relevantAssignments);
+            }
+            return result;
+        }
+    }
+}
