024fae6c3fdd4a5956fe6cf24eaf56e7b8feb194
Andy Wojciechowski <wojciechowskia@msoe.edu>
2017-12-08 10:23:02 -0600

    Converted Grade Calculator to WPF. Still need to add the reference to Interop.Excel

---
+++ GradeCalculator.WPF/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+    public class Assignment
+    {
+        public string Name { get; set; }
+        public GradeCategory category { get; set; }
+        public double TotalPointsEarned { get; set; }
+        public double TotalPossiblePoints { get; set; }
+    }
+}
---
+++ GradeCalculator.WPF/GradeCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+    public class GradeCategory
+    {
+        public string Name { get; set;  }
+        public double CategoryWeight { get; set; }
+    }
+}
