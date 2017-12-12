892466220361cccb38e3db1a9a5fc2f654488adc
andy-wojciechowski <31997141+andy-wojciechowski@users.noreply.github.com>
2017-12-12 11:35:10 -0600

    Initial implementation of the edit category window.

--- GradeCalculator/Presenters/EditCategoryPresenter.cs
+++ GradeCalculator/Presenters/EditCategoryPresenter.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GradeCalculator.Model;
 
 namespace GradeCalculator.Presenters
 {
     public class EditCategoryPresenter : BasePresenter<EditCategoryForm>
     {
-        public EditCategoryPresenter(EditCategoryForm form) : base(form) { }
+        private GradeCategory category { get; set; }
+        public EditCategoryPresenter(EditCategoryForm form, GradeCategory category) : base(form)
+        {
+            this.category = category;
+        }
+
+        public void InitializeDataBinding()
+        {
+            this.View.InitializeDataBinding(category);
+        }
     }
 }
--- GradeCalculator/Windows/EditCategoryForm.xaml.cs
+++ GradeCalculator/Windows/EditCategoryForm.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GradeCalculator.Model;
+using GradeCalculator.Presenters;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using GradeCalculator.Model;
 
 namespace GradeCalculator.Windows
 {
@@ -20,9 +11,39 @@ namespace GradeCalculator.Windows
     /// </summary>
     public partial class EditCategoryForm : Window
     {
+        private EditCategoryPresenter presenter { get; set; }
         public EditCategoryForm(GradeCategory category)
         {
             InitializeComponent();
+            this.presenter = new EditCategoryPresenter(this, category);
+            this.presenter.InitializeDataBinding();
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Figure out if the object in edit class form will need to be updated
+            this.Close();
+        }
+
+        public void InitializeDataBinding(GradeCategory category)
+        {
+            //Name Property
+            Binding binding1 = new Binding();
+            binding1.Source = category;
+            binding1.Path = new PropertyPath("Name");
+            binding1.Mode = BindingMode.TwoWay;
+            binding1.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+
+            //Worth Property
+            Binding binding2 = new Binding();
+            binding2.Source = category;
+            binding2.Path = new PropertyPath("CategoryWorth");
+            binding2.Mode = BindingMode.TwoWay;
+            binding2.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+
+            //Set the 2 bindings
+            BindingOperations.SetBinding(nameTextBox, TextBox.NameProperty, binding1);
+            BindingOperations.SetBinding(worthTextBox, TextBox.NameProperty, binding2);
         }
     }
 }
